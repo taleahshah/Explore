194bb637dbf01bce965c2e69b1429dcb2a03143c
Taleah Shah <51045892+taleahshah@users.noreply.github.com>
2020-12-15 13:46:38 +0500

    Create Home.js

---
+++ src/components/tabs/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../../App.css';
+import Cards from '../Cards';
+import Footer from '../Footer';
+import MainSection from '../MainSection';
+
+
+function Home() {
+    return (
+        <>
+            <MainSection />
+            <Cards />
+            <Footer />
+
+        </>
+    );
+}
+
+export default Home;
